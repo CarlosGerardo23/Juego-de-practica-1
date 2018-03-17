33a21daab6317fe5cb8f7db805c668a8a15f63eb
CarlosGerardo23 <carlos.enriquez@saei.mx>
2018-03-16 22:25:30 -0600

    Add proyect

---
+++ GameJam practica1/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+  Rigidbody rb;
+  public float speed = 3f;
+
+  // Use this for initialization
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKey(KeyCode.W))
+      transform.Translate(speed * Time.deltaTime, 0, 0);
+    if (Input.GetKey(KeyCode.S))
+      transform.Translate(-speed * Time.deltaTime, 0, 0);
+    if (Input.GetKey(KeyCode.W))
+      transform.Translate(0, 0, speed * Time.deltaTime);
+    if (Input.GetKey(KeyCode.D))
+      transform.Translate(0, 0, -speed * Time.deltaTime);
+
+    
+  }
+}
